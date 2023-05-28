c8dde99e6cf12814656306e84eba93bbd441baac
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-05-28 12:48:56 +0330

    enable asgi for channels

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from intro.chat.urls import urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "websocket": AllowedHostsOriginValidator(
+        URLRouter(urlpatterns)
+    )
+})
