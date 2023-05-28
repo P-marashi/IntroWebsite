e0214cd55e4c6a3e70a7d0380133963536a8c309
pouyamarashii <119898085+pouyamarashii@users.noreply.github.com>
2023-05-28 13:31:59 +0430

    repaire the support urls and blog urls

--- intro/blog/urls.py
+++ intro/blog/urls.py
@@ -7,11 +7,11 @@ from .apis import (
 
 urlpatterns = [
     # Endpoint for creating a new blog post (admin only)
-    path('blog/', BlogPostCreateAPIView.as_view(), name='blog-create'),
+    path('', BlogPostCreateAPIView.as_view(), name='blog-create'),
 
     # Endpoint for retrieving a list of all blog posts
-    path('blog/list/', BlogPostListAPIView.as_view(), name='blog-list'),
+    path('list/', BlogPostListAPIView.as_view(), name='blog-list'),
 
     # Endpoint for retrieving, updating, and deleting a specific blog post (admin only)
-    path('blog/<slug:slug>/', BlogPostDetailAPIView.as_view(), name='blog-detail'),
+    path('<slug:slug>/', BlogPostDetailAPIView.as_view(), name='blog-detail'),
 ]
