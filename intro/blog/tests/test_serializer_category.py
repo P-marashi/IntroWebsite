c9c1cee538a1397e7909561aa8fab75c7d6c3549
pouyamarashii <119898085+pouyamarashii@users.noreply.github.com>
2023-06-07 16:04:23 +0430

    add comment in test_serializer_category .

--- intro/blog/tests/test_serializer_category.py
+++ intro/blog/tests/test_serializer_category.py
@@ -6,18 +6,26 @@ from myapp.models import BlogPost, Category
 @pytest.fixture
 def sample_category_data():
     return {
-        "name": "test Serialzer"
+        "name": "test Serializer"
     }
 
 
-def test_category_serialzer(sample_category_data):
+def test_category_serializer_valid_data(sample_category_data):
+    # Create an instance of the CategorySerializer with sample data
     serializer = CategorySerializer(data=sample_category_data)
+
+    # Assert that the serializer is valid and the validated data matches the sample data
     assert serializer.is_valid()
     assert serializer.validated_data == sample_category_data
 
 
-def test_category_serializer_misssing_required_field(sample_category_data):
+def test_category_serializer_missing_required_field(sample_category_data):
+    # Remove the 'name' field from the sample data
     del sample_category_data['name']
+
+    # Create an instance of the CategorySerializer with modified sample data
     serializer = CategorySerializer(data=sample_category_data)
+
+    # Assert that the serializer is not valid and 'name' field is present in the errors
     assert not serializer.is_valid()
-    assert "title" in serializer.errors
+    assert 'name' in serializer.errors
