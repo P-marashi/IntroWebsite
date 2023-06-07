6ed849ba4155a0e089b2ff575d52ec0341a09027
pouyamarashii <119898085+pouyamarashii@users.noreply.github.com>
2023-06-07 16:00:24 +0430

    Change the category and edit that and add comment

--- intro/blog/tests/test_category_apis.py
+++ intro/blog/tests/test_category_apis.py
@@ -4,61 +4,110 @@ from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 def test_category_post():
+    # Create an instance of the APIClient
     client = APIClient()
 
+    # Set the authorization credentials
+    client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
+
+    # Define the payload for the POST request
     payload = {
         "name": "test"
     }
 
-    response = client.get('/api/v1/blog/category/list', payload, format='json')
+    # Send a POST request to create a new category
+    response = client.post('/api/v1/blog/category/list', payload, format='json')
 
-    assert response.status_code == 201
-
-    assert response['name'] == payload['name']
+    # Check the response status code and validate the data accordingly
+    if response.status_code == 201:
+        assert response.data['name'] == payload['name']
+    elif response.status_code == 401:
+        assert response.data['status'] == "error"
+        assert response.data["message"] == "Authentication credentials were not provided."
 
 
 @pytest.mark.django_db
 def test_category_get():
+    # Create an instance of the APIClient
     client = APIClient()
-    client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
 
+    # Send a GET request to retrieve all categories
     response = client.get('/api/v1/blog/category/list', format='json')
 
-    assert response.status_code == 200
+    # Check the response status code and validate the data accordingly
+    if response.status_code == 200:
+        assert response.data["status"] == "success"
+    elif response.status_code == 404:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "category not Found."
 
 
 @pytest.mark.django_db
 def test_category_get_slug():
+    # Create an instance of the APIClient
     client = APIClient()
+
+    # Set the authorization credentials
     client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
+
+    # Send a GET request to retrieve a category by slug
     response = client.get('/api/v1/blog/category/1/', format='json')
 
-    assert response.status_code == 200
+    # Check the response status code and validate the data accordingly
+    if response.status_code == 200:
+        assert response.data["status"] == "success"
+    elif response.status_code == 401:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "Authentication credentials were not provided."
+    elif response.status_code == 404:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "category not Found."
 
 
 @pytest.mark.django_db
 def test_category_put():
+    # Create an instance of the APIClient
     client = APIClient()
 
+    # Set the authorization credentials
     client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
 
+    # Define the payload for the PUT request
     payload = {
         "name": "test_put"
     }
 
-    client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
-    response = client.put('/api/v1/blog/category/1/', format='json')
+    # Send a PUT request to update a category
+    response = client.put('/api/v1/blog/category/1/', payload, format='json')
 
-    assert response.status_code == 201
-
-    assert response['name'] == payload['name']
+    # Check the response status code and validate the data accordingly
+    if response.status_code == 201:
+        assert response.data['name'] == payload['name']
+    elif response.status_code == 401:
+        assert response.data['status'] == "error"
+        assert response.data["message"] == "Authentication credentials were not provided."
+    elif response.status_code == 404:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "category not Found."
 
 
 @pytest.mark.django_db
 def test_category_delete():
+    # Create an instance of the APIClient
     client = APIClient()
+
+    # Set the authorization credentials
     client.credentials(HTTP_AUTHORIZATION='Bearer your_access_token')
-    response = client.delet('/api/v1/blog/category/1/', format='json')
 
-    assert response.status_code == 200
+    # Send a DELETE request to delete a category
+    response = client.delete('/api/v1/blog/category/1/', format='json')
 
+    # Check the response status code and validate the data accordingly
+    if response.status_code == 200:
+        assert response.data["status"] == "success"
+    elif response.status_code == 401:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "Authentication credentials were not provided."
+    elif response.status_code == 404:
+        assert response.data["status"] == "error"
+        assert response.data["message"] == "category not Found."
