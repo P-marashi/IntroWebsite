b9cdfff67486b768ede666a54c2b613ce4040c4c
pouyamarashii <119898085+pouyamarashii@users.noreply.github.com>
2023-06-07 16:03:01 +0430

    Change the test_serializer_blog  and add comment.

--- intro/blog/tests/test_serializer_blog.py
+++ intro/blog/tests/test_serializer_blog.py
@@ -14,15 +14,21 @@ def sample_blog_post_data():
 
 
 def test_blog_post_serializer_valid_data(sample_blog_post_data):
+    # Create an instance of the BlogPostSerializer with sample data
     serializer = BlogPostSerializer(data=sample_blog_post_data)
+
+    # Assert that the serializer is valid and the validated data matches the sample data
     assert serializer.is_valid()
     assert serializer.validated_data == sample_blog_post_data
 
 
 def test_blog_post_serializer_missing_required_field(sample_blog_post_data):
+    # Remove the 'title' field from the sample data
     del sample_blog_post_data['title']
+
+    # Create an instance of the BlogPostSerializer with modified sample data
     serializer = BlogPostSerializer(data=sample_blog_post_data)
+
+    # Assert that the serializer is not valid and 'title' field is present in the errors
     assert not serializer.is_valid()
     assert 'title' in serializer.errors
-
-# Add more test functions as needed for other serializer methods or scenarios
