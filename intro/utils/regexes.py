eacd315bd75ce13872fa6330b70e2a3ad4f20f54
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-06-08 17:53:40 +0330

    regexes fixed

--- intro/utils/regexes.py
+++ intro/utils/regexes.py
@@ -1,8 +1,9 @@
 # TODO: Add regexes
 import re
 
-PHONE_NUMBER_REGEX = r""
-EMAIL_REGEX = r""
+PHONE_NUMBER_REGEX = \
+    r"[\+\d]?(\d{2,3}[-\.\s]??\d{2,3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+EMAIL_REGEX = r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
 
 
 def is_phone_or_email(method):
@@ -14,21 +15,12 @@ def is_phone_or_email(method):
     it should be phonenumber or email address
     Returns:
     --------
-    str-> "phone" | "email"
-    bool-> False 
+    str -> "phone" | "email"
+    bool -> False
     when condition is False the False value will return
     """
-    # if re.match(PHONE_NUMBER_REGEX, method):
-        # return "phone"
-    # elif re.match(EMAIL_REGEX, method):
-        # return "email"
-    # return False
-
-    try:
-        phone = int(method)
+    if re.match(PHONE_NUMBER_REGEX, method):
         return "phone"
-    except:
-        email = method
+    elif re.match(EMAIL_REGEX, method):
         return "email"
-    finally:
-        return "email"
+    return False
